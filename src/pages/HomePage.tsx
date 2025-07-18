17455456de4a8f8172a156f92ee7b9ca8bbd4fa0
tabishghouri <tabishghouri3@gmail.com>
2025-07-17 23:31:17 -0400

    added gradient line seperator

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,6 +7,7 @@ const HomePage: React.FC = () => {
   return (
     <div className='min-h-screen'>
       <Landing />
+      <div className='w-full h-px bg-gradient-to-r from-transparent via-purple-500/40 to-transparent my-4 md:my-8 mx-auto max-w-7xl' />
       <div className='max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8'>
         <EventsSection />
         <NewsSection />
